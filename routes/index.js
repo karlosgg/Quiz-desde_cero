1da39de9f5b19379f83376ef1e0da59dd0c4aed4
Jose Grijalva <karlosgg@gmail.com>
2015-07-05 18:12:31 -0600

    Modificaciones solicitadas (autor)

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET author page. */
+
+router.get('/author', function(req, res) {
+  res.render('author', { author: 'José Carlos Grijalva González' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
