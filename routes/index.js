21dd6607e926fb92a9b59a063319e938a24ee54d
Jose Grijalva <karlosgg@gmail.com>
2015-08-16 14:31:07 -0600

    Busqueda de preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos :quizId
+router.param('quizId', quizController.load);
+
 /* GET author page */
 
 router.get('/author', function(req, res) {
