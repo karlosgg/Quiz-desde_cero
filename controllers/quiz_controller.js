f5610e96c81ed617ee2df559ce742c50cc50a7f6
Jose Grijalva <karlosgg@gmail.com>
2015-08-16 23:21:23 -0600

    Crear, editar y borar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -25,7 +25,7 @@ exports.index = function(req, res){
   else{
   models.Quiz.findAll().then(
     function(quizes){
-      res.render('quizes/index',{ quizes: quizes});
+      res.render('quizes/index.ejs',{ quizes: quizes, errors: []});
     }
   ).catch(function(error) { next(error);})
 }
@@ -33,7 +33,7 @@ exports.index = function(req, res){
 
 //GET /quizes/:id
 exports.show = function(req, res){
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 //GET /quizes/:id/answer
@@ -42,6 +42,70 @@ exports.answer = function(req, res){
 	if(req.query.respuesta === req.quiz.respuesta){
 	  resultado = 'Correcto';
 	}
-	res.render('quizes/answer',{ quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer',
+           { quiz: req.quiz,
+             respuesta: resultado,
+             errors:[]
+        });
     };
  
+//GET /quizes/new
+exports.new=function(req, res){
+  var quiz=models.Quiz.build(//crea objeto quiz
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+  res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+//POST /quizes/create
+exports.create=function(req, res){
+  var quiz=models.Quiz.build(req.body.quiz);
+    
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if(err){
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      }else{
+        quiz //guardar en DB los campos pregunta y respuesta de quiz
+        .save({fields:["pregunta", "respuesta"]})
+        .then(function(){ res.redirect('/quizes')}); 
+      }//Redireccion HTTP (URL Relativo) lista de preguntas
+    }
+  );
+};
+
+
+//GET /quizes/:id/edit
+exports.edit = function(req, res){
+  var quiz = req.quiz; //autoload de instancia de quiz
+  res.render('quizes/edit', {quiz: quiz, error: errors});
+};
+
+//PUT /quizes/:id
+exports.update = function(req, res){
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.pregunta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if(err){
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      }else{
+        req.quiz //guardar en DB los campos pregunta y respuesta de quiz
+        .save({fields:["pregunta", "respuesta"]})
+        .then(function(){ res.redirect('/quizes')}); 
+      }//Redireccion HTTP (URL Relativo) lista de preguntas
+    }
+  );
+};
+
+//DELETE /quizes/:id
+exports.destroy = function(req, res){
+  req.quiz.destroy().then( function(){
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
